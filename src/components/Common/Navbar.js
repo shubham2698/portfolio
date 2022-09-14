cd1127015917641865a95616a750be93c2dbee8b
Shubham Joshi <shubhamjoshi199826@gmail.com>
2022-09-15 02:08:21 +0530

    testing commit

---
+++ src/components/Common/Navbar.js
@@ -0,0 +1,12 @@
+import React,{Component} from "react";
+
+export class Navbar extends Component {
+
+    render(){
+        return(
+            <div className="container">
+                <h1>Navbar</h1>
+            </div>
+        )
+    }
+}
